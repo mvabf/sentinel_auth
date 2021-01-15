fae75a8d20b2085b4ba71ff71a038df467600027
Marcus Baltazar <mvabf_dev@outlook.com>
2021-01-15 11:44:48 -0300

    projeto criado, rota register

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Sentinel;
+use Illuminate\Http\Request;
+
+
+class RegistrationController extends Controller
+{
+    public function register() {
+        return view('authentication.register');
+    }
+
+    public function postRegister(Request $request) {
+        $user  = Sentinel::registerAndActivate($request->all());
+        
+        return redirect('/');
+    }
+}
---
+++ resources/views/authentication/register.blade.php
@@ -0,0 +1,29 @@
+<style>
+    form {
+        margin-top: 10%;
+        padding-left: 40%;
+    }
+</style>
+
+<form action="/register" method="POST">
+    @csrf
+    <div>
+       <input type="email" name="email" placeholder="example@example.com"> 
+    </div>
+    <div>
+      <input type="text" name="first_name" placeholder="First Name">  
+    </div>
+    <div>
+       <input type="text" name="last_name" placeholder="Last Name"> 
+    </div>
+    <div>
+       <input type="password" name="password" placeholder="Password"> 
+    </div>
+    <div>
+        <input type="password" name="password_confirmation" placeholder="Confirm Password">
+    </div>
+    <div>
+        <input type="submit">
+    </div>
+    
+</form>
